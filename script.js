bc92c187c3213c5b4a043f3c90a5cb75e7449afd
Daniele Mastroti <daniele.mastroti@intesasanpaolo.com>
2026-07-08 09:29:46 +0200

    new pages

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", async () => {
+
     try {
+
         const response = await fetch("viaggio.json");
+
+        if (!response.ok) {
+            throw new Error(`Errore caricamento JSON: ${response.status}`);
+        }
+
         const dati = await response.json();
 
         document.getElementById("titolo").textContent = dati.titolo;
@@ -14,7 +21,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
             giornoDiv.innerHTML = `
                 <h2>${giorno.giorno}</h2>
-                <p><strong>Data:</strong> ${giorno.data}</p>
+                <div class="data">${giorno.data}</div>
             `;
 
             creaSezione("Mattino", giorno.mattino, giornoDiv);
@@ -24,30 +31,3 @@ document.addEventListener("DOMContentLoaded", async () => {
             home.appendChild(giornoDiv);
         });
 
-    } catch (errore) {
-        console.error("Errore caricamento JSON:", errore);
-    }
-});
-
-
-function creaSezione(titolo, attivita, contenitore) {
-
-    if (!attivita || attivita.length === 0) return;
-
-    const sezione = document.createElement("div");
-    sezione.className = "sezione";
-
-    const htmlAttivita = attivita.map(item => `
-        <div class="attivita">
-            <strong>${item.ora}</strong> - ${item.nome}
-            ${item.maps ? ` | ${item.maps}>` : ""}
-        </div>
-    `).join("");
-
-    sezione.innerHTML = `
-        <h3>${titolo}</h3>
-        ${htmlAttivita}
-    `;
-
-    contenitore.appendChild(sezione);
-}
