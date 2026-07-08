f17bbcd9504c87a6dc47a75aba889672192518a2
Daniele Mastroti <daniele.mastroti@intesasanpaolo.com>
2026-07-08 08:36:29 +0200

    new pages

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-fetch('viaggio.json').then(r=>r.json()).then(d=>{document.getElementById('title').textContent=d.titolo;let root=document.getElementById('days');d.giorni.forEach(g=>{let day=document.createElement('div');day.className='day';let h=document.createElement('div');h.className='h';h.textContent='Giorno '+g.giorno;let c=document.createElement('div');c.className='c';h.onclick=()=>c.classList.toggle('open');['mattino','pomeriggio','sera'].forEach(s=>{c.innerHTML+='<h3>'+s+'</h3>'; (g[s]||[]).forEach(t=>{c.innerHTML+=`<div><b>${t.ora}</b> ${t.nome} <a target=_blank href="${t.maps}">Maps</a></div>`})});day.append(h,c);root.append(day);});});
+fetch('viaggio.json').then(r=>r.json()).then
+(d=>{document.getElementById('title').textContent=d.titolo;
+    let root=document.getElementById('days');
+    d.giorni.forEach(g=>{let day=document.createElement('div');
+        day.className='day';
+        let h=document.createElement('div');
+        h.className='h';h.textContent='Giorno '+g.giorno;
+        let c=document.createElement('div');
+        c.className='c';
+        h.onclick=()=>c.classList.toggle('open');
+        ['mattino','pomeriggio','sera'].forEach(s=>{c.innerHTML+='<h3>'+s+'</h3>';
+             (g[s]||[]).forEach(t=>{c.innerHTML+=`<div><b>${t.ora}</b> ${t.nome} <a target=_blank href="${t.maps}">Maps</a></div>`})});
+             day.append(h,c);root.append(day);});});
